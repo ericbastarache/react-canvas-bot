065c04c17c3b7bf6529838ebae99b6e0124b3667
Eric Bastarache <eric_bastarache@hotmail.com>
2017-07-23 16:43:32 -0400

    Refactored oval component

--- src/components/Oval/Oval.js
+++ src/components/Oval/Oval.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Oval.css';
 
-const Oval = (props) => {
-  return (
-    <div>Hello</div>
-  );
+import { Button } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+
+class Oval extends Component {
+  handleClick = (e) => {
+    this.props.setOvalActive(e.target.value);
+  }
+
+  render() {
+    const value = this.props.ovalActive;
+    return (
+      <Button onClick={this.handleClick} value={value}>
+        <FontAwesome
+          name='circle'
+          />
+      </Button>
+    )
+  }
 }
 
 export default Oval;
