cc2287ded56ed04f13499736f87321dfdcbc0401
Eric Bastarache <eric_bastarache@hotmail.com>
2017-07-23 16:43:08 -0400

    Updated brush size through brush component

--- src/components/Brush/Brush.js
+++ src/components/Brush/Brush.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Brush.css';
 
-const Brush = (props) => {
-  return (
-    <div>Hello</div>
-  );
+class Brush extends Component {
+
+  handleChange = (e) => {
+    this.props.brushSizeChange(e.target.value);
+  }
+
+  render () {
+    const value = this.props.lineWidth;
+    return (
+      <div>
+        <label htmlFor="brush-size">
+          Brush Size
+        </label>
+        <input name="brush-size" className="brushsize" onChange={this.handleChange} type="range" min="0" max="20" step="1" value={value} />
+      </div>
+    );
+  }
 }
 
 export default Brush;
