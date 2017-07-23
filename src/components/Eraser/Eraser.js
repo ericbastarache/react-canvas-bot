a597a7ba41f6ce1520afe15afe9dc497e0cefa53
Eric Bastarache <eric_bastarache@hotmail.com>
2017-07-23 18:28:37 -0400

    Added event handler to eraser

--- src/components/Eraser/Eraser.js
+++ src/components/Eraser/Eraser.js
@@ -5,6 +5,10 @@ import { Button } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 class Eraser extends Component {
+
+  handleClick = (e) => {
+    this.props.eraseCanvas();
+  }
   render() {
     return (
       <Button onClick={this.handleClick}>
