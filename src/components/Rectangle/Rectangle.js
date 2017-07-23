d7b2eb29acea28f793b7de9aa684cd5657c35c47
Eric Bastarache <eric_bastarache@hotmail.com>
2017-07-23 16:43:41 -0400

    Refactored rectangle component

--- src/components/Rectangle/Rectangle.js
+++ src/components/Rectangle/Rectangle.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Rectangle.css';
 
-const Rectangle = (props) => {
-  return (
-    <div>Hello</div>
-  );
+import { Button } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+
+class Rectangle extends Component {
+  handleClick = (e) => {
+    this.props.setRectActive(e.target.value);
+  }
+
+  render() {
+    const value = this.props.rectActive;
+    return (
+      <Button onClick={this.handleClick} value={value}>
+        <FontAwesome
+          name='square'
+          />
+      </Button>
+    );
+  }
 }
 
 export default Rectangle;
